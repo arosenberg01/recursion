bcedd8faa4ef87ebc2e054ab8097e30afe7db0d3
Ansel Rosenberg <ansel01@gmail.com>
2015-02-24 23:33:49 -0800

    Consolidate multiple case-handling branches

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
@@ -6,99 +6,88 @@
 // but you don't so you're going to write it from scratch:
 
 var y = {
-	prop1: "a",
-	prop2: {
-		prop3: "b",
-		prop4: {
-			prop5: "c",
-			prop6: "d"
-		}
-	}
+  prop1: "a",
+  propObjX: {
+    prop2: "b",
+    propObjY: {
+      prop3: "c",
+      prop4: "d"
+    }
+  }
 }
 
-var num = 3;
-var string = "Hello world";
-var bool = true;
-var none = null;
-var undef = undefined;
-var func = function() { return 3 };
-
-
 var testObj = {
-	num: 3,
-	string: "Hello world",
-	bool: true,
-	none: null,
-	undef: undefined,
+  num: 3,
+  string: "Hello world",
+  bool: true,
+  none: null,
+  undef: undefined,
+  func: function() { return 3 },
+  arr: [1, 2],
+  obj: y
 };
-
-var testArr = [num, string, bool, none, undef, func];
 
 var stringifyJSON = function(input, accumulator) {
   if (accumulator === undefined) {
-  	accumulator = "";
+    accumulator = '';
   }
-  
-  // console.log("Accum-out: " + accumulator);
 
-	if (Array.isArray(input)) {
+  if (typeof input === 'object') {
+    if (Array.isArray(input)) {
+    // console.log(input + ' = array');
 
-	} else if (input === 'undefined') {
+    // var front = '[';
+    // var back = ']'
 
-	} else if (typeof input === 'object') {
-  	for (var key in input) {
-  		
-  		if (typeof input[key] === 'object') {
-  			console.log('nested obj');
+   //  for (var i = 0; i < input.length; i++) {
+   //   return stringifyJSON(input[i], accumulator)
+    //   }
 
-  		  return stringifyJSON(input[key], accumulator);
-  		} else {
-        accumulator += key + " -- " + input[key] + ", ";
-        console.log("Accum-in: " + accumulator);
-  		}
-  	}
   } else {
-  	accumulator += "" + input;
-  }
-  
- return accumulator;
+      for (var key in input) {
+        if (typeof input[key] === 'object') {
+          console.log('nested obj');
+          return stringifyJSON(input[key], accumulator);
+        } else {
+          accumulator += key + " -- " + input[key] + ", ";
+          console.log("Accum-in: " + accumulator);
+        }
+      }
+    }
 
-};
-
-
-var stringifyTest = function(input, accumulator) {
-  if (accumulator === undefined) {
-  	accumulator = '';
-  }
-
-  if (typeof input === 'boolean') {
-    console.log(input + ' = boolean');
   } else if (typeof input === 'string') {
-  	console.log(input + ' = string');
-  } else if (typeof input === 'number') {
-  	console.log(input + ' = number');
-  } else if (input === null) {
-  	console.log(input + ' = null');
-  } else if (input === undefined) {
-  	console.log(input + ' = undefined');
-  } else if (Array.isArray(input)) {
-  	console.log(input + ' = array');
-  } else if (typeof input === 'function') {
-  	console.log(input + ' = function');
+    var result = '\'\"' + input + '\"\'';
+    console.log(input + ' (' + typeof input + ')' + ' to ' + result + ' (' + typeof result + ')' );
+    accumulator += ' ' + result + ' ';
+
+  } else if (typeof input === 'boolean' || typeof input === 'number' || input === null) {
+    var result = '\'' + input + '\'';
+    console.log(input + ' (' + typeof input + ')' + ' to ' + result + ' (' + typeof result + ')' );
+    accumulator += ' ' + result + ' ';
   } else {
-  	console.log (input + ' ?= An object?');
-  }
+    console.log (input + ' = ???');
+  } 
+
+  return accumulator;
 };
 
 
 
+var run = function() {
+  for (var key in testObj) {
+    stringifyJSON(testObj[key]);
+  }
+}();
 
 
 
-// console.log("Return: " + stringifyJSON(y));
 
-for (var i = 0; i < testArr.length; i++) {
-	// console.log('' + testArr[i] + ' = ' + stringifyTest(testArr[i]));
-	stringifyTest(testArr[i]);
-}
+
+
+
+
+
+
+
+
 
